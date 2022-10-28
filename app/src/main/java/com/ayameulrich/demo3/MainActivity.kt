fe2a3b7d2b202a5cb32271daf2176ffbdbb3bd83
cheetoyumyum <chet.hawkins.met@gmail.com>
2022-10-27 23:56:32 -0500

    Added some buttons just to show that we have binding and layout inflator working. build.gradle has been updated to add binding
    misc.xml might have an error with the changes as it is referencing
    "users/scsol/" Which may not match up to everyone elses source.
    Maybe this is generated when building gradle?

--- app/src/main/java/com/ayameulrich/demo3/MainActivity.kt
+++ app/src/main/java/com/ayameulrich/demo3/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.ayameulrich.demo3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ayameulrich.demo3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Start layoutInflating.
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // Application code starts here
+        binding!!.tvTextView.text = "Hello! Welcome to layout inflation"
+
+        binding!!.btnBtn.setOnClickListener() {
+            binding!!.tvTextView.text = "you pressed the Hello button! Hi!"
+        }
+
     }
 }
