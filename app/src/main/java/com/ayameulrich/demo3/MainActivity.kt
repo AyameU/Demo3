3c86df8a11900c5e11030c5681a3e662d436d2a2
Jordan Landry <LandryJRDN@gmail.com>
2022-11-07 20:50:42 -0600

    Quick and dirty recycler view

--- app/src/main/java/com/ayameulrich/demo3/MainActivity.kt
+++ app/src/main/java/com/ayameulrich/demo3/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ayameulrich.demo3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ayameulrich.demo3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var binding : ActivityMainBinding? = null
+    private val itemsList = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +25,19 @@ class MainActivity : AppCompatActivity() {
             binding!!.tvTextView.text = "you pressed the Hello button! Hi!"
         }
 
+        binding!!.AddItemBtn.setOnClickListener() {
+            addItem()
+        }
+
+        val recyclerView = binding!!.recyclerView
+        customAdapter = CustomAdapter(itemsList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+    }
+
+    private fun addItem() {
+        itemsList.add("Item")
+        customAdapter.notifyDataSetChanged()
     }
 }
